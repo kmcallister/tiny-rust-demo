a5e0e09f183c0fbb859db65680366b397ca6868c
Melvin Kicchi <melvin@likes.pounc.es>
2015-10-06 14:30:24 +0200

    Upgrade to rustc 1.5.0-nightly (32a8567ea 2015-10-05) (built 2015-10-06)

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-for d in rustc git cargo ar ld objcopy nasm; do
+for d in rustc git cargo ar ld objcopy nasm hexdump; do
     which $d >/dev/null || (echo "Can't find $d, needed to build"; exit 1)
 done
 
-printf "Tested on rustc 1.1.0-dev (435622028 2015-05-04)\nYou have  "
+printf "Tested on rustc 1.5.0-nightly (32a8567ea 2015-10-05)\nYou have  "
 rustc --version
 echo
 
@@ -22,17 +22,17 @@ rustc tinyrust.rs \
     -O -C no-stack-check -C relocation-model=static \
     -L syscall.rs/target/release
 
-ar x libtinyrust.rlib tinyrust.o
+ar x libtinyrust.rlib tinyrust.0.o
 
-objdump -dr tinyrust.o
+objdump -dr tinyrust.0.o
 echo
 
-ld --gc-sections -e main -T script.ld -o payload tinyrust.o
+ld --gc-sections -e main -T script.ld -o payload tinyrust.0.o
 objcopy -j combined -O binary payload payload.bin
 
 ENTRY=$(nm -f posix payload | grep '^main ' | awk '{print $3}')
 nasm -f bin -o tinyrust -D entry=0x$ENTRY elf.s
 
 chmod +x tinyrust
-hd tinyrust
+hexdump -C tinyrust
 wc -c tinyrust
