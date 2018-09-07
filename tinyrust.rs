0c8d3412961c14490539a378fa0bb9cfbd1eabb0
Torbjørn Birch Moltu <t.b.moltu@lyse.net>
2018-09-07 19:48:22 +0200

    Use the stable hint::unreachable_unchecked()
    Still requires nightly because sc uses inline_asm
    
    Also remove identity transmute left over from 8c711fca.

--- tinyrust.rs
+++ tinyrust.rs
@@ -1,14 +1,14 @@
 #![crate_type="rlib"]
-#![feature(core_intrinsics)]
+#![no_std]
 
 #[macro_use] extern crate sc;
 
-use std::{mem, slice, intrinsics};
+use core::{hint,slice};
 
 fn exit(n: usize) -> ! {
     unsafe {
         syscall!(EXIT, n);
-        intrinsics::unreachable()
+        hint::unreachable_unchecked()
     }
 }
 
@@ -22,9 +22,7 @@ fn write(fd: usize, buf: &[u8]) {
 pub fn main() {
     // Make a Rust value representing the string constant we stashed
     // in the ELF file header.
-    let message: &'static [u8] = unsafe {
-        mem::transmute(slice::from_raw_parts(0x00400008 as *const u8, 7))
-    };
+    let message = unsafe{ slice::from_raw_parts(0x00400008 as *const u8, 7) };
 
     write(1, message);
     exit(0);
